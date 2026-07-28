4f6065af68a3a8d59bc13d65a0cbc1b8624540c1
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-07-28 21:11:52 +0600

    added counter condition

--- counter.js
+++ counter.js
@@ -7,9 +7,21 @@ let counterCount = 0;
 plusButton.addEventListener("click", () => {
   counterCount = counterCount + 1;
   counterElement.textContent = counterCount;
+
+  if (counterCount >= 10) {
+    plusButton.setAttribute("disabled", true);
+  } else {
+    plusButton.removeAttribute("disabled", false);
+  }
 });
 
 minusButton.addEventListener("click", () => {
   counterCount = counterCount - 1;
   counterElement.textContent = counterCount;
+
+  if (counterCount <= 0) {
+    minusButton.setAttribute("disabled", true);
+  } else {
+    minusButton.removeAttribute("disabled", false);
+  }
 });
